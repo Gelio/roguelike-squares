df7e4606921baa8d38502847da374ba63a4d0beb
Gelio <voreny.gelio@gmail.com>
2016-02-20 23:29:09 +0100

    added a restart button

--- app/scripts/components/Game.js
+++ app/scripts/components/Game.js
@@ -23,6 +23,10 @@ export default class Game extends React.Component {
                 <StatsBar player={this.game.player} floor={this.game.floor} gameOver={this.game.gameOver} newGame={this.newGame.bind(this)} />
 
                 <Map gameMap={this.game.gameMap} handleMove={this.game.handleMove.bind(this.game)} forceUpdate={this.forceUpdate.bind(this)} />
+
+                <div class="text-xs-center m-t-1">
+                    <button class="btn btn-primary" onClick={this.newGame.bind(this)}>Restart</button>
+                </div>
             </div>
         );
     }
